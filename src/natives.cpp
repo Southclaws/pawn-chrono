7a2fedc372afe7d258282ff8fa5b60dd3441113a
Southclaws <southclawjk@gmail.com>
2018-06-02 13:07:38 +0100

    implemented TimeParse written TimeParse ISO6801 test started work on convertion operators

--- src/natives.cpp
+++ src/natives.cpp
@@ -14,9 +14,20 @@ cell Natives::TimeFormat(AMX* amx, cell* params)
 	return amx_SetCppString(amx, params[3], output, params[4]);
 }
 
+// const string[], const fmt[], &Timestamp:output
 cell Natives::TimeParse(AMX* amx, cell* params)
 {
-    // todo
+	std::string string = amx_GetCppString(amx, params[1]);
+	std::string fmt = amx_GetCppString(amx, params[2]);
+	cell *output;
+	amx_GetAddr(amx, params[3], &output);
+
+	std::istringstream is(string);
+	date::sys_seconds d;
+
+	date::from_stream(is, fmt.c_str(), d);
+
+	*output = d.time_since_epoch().count();
 
     return 0;
 }
